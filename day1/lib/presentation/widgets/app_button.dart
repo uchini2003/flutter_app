d50f101e6e24f3dc86891e6ccaca08d1d21f0af3
uchini2003 <uchinimarasinghe561@gmail.com>
2025-04-22 00:57:09 +0530

    button look and text

--- day1/lib/presentation/widgets/app_button.dart
+++ day1/lib/presentation/widgets/app_button.dart
@@ -24,3 +24,28 @@ class AppButton extends StatelessWidget {
     this.textColor,
   });
 
+    @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      // btn look
+      style: ElevatedButton.styleFrom(
+        backgroundColor: backgroundColor ?? Theme.of(context).primaryColor,
+        foregroundColor: textColor ?? Colors.white,
+        padding: const EdgeInsets.symmetric(vertical: 16.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8.0),
+        ),
+      ),
+      //btn text
+      child: Text(
+        text,
+        style: const TextStyle(
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
+
