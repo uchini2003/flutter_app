255c162e211dff44b422664c4a3fb63117803605
uchini2003 <uchinimarasinghe561@gmail.com>
2025-04-21 15:32:48 +0530

    remove screen from nav

--- day1/lib/presentation/screens/splash_screen.dart
+++ day1/lib/presentation/screens/splash_screen.dart
@@ -1,9 +1,24 @@
 import 'package:flutter/material.dart';
 import 'welcome_screen.dart';
 
+//splash screen nav for 2 sec
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
   @override
   State<SplashScreen> createState() => _SplashScreenState();
 }
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _navigateToWelcome();
+  }
+
+
+   //remove screen from nav
+  _navigateToWelcome() async {
+    await Future.delayed(const Duration(seconds: 2));
+
+
