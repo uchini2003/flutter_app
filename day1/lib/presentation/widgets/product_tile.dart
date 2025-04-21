4b4ace69d5ecbde7a9321de56288ebac40faba8b
uchini2003 <uchinimarasinghe561@gmail.com>
2025-04-22 01:01:08 +0530

    product image box

--- day1/lib/presentation/widgets/product_tile.dart
+++ day1/lib/presentation/widgets/product_tile.dart
@@ -4,4 +4,36 @@ import '../screens/home_screen.dart';
 //depicts one product in list
 class ProductTile extends StatelessWidget {
   final Product product;
+  final Product product;
+
+  const ProductTile({
+    super.key,
+    required this.product,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 8.0),
+      elevation: 2.0,
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            //Product pic box
+            Container(
+              width: 80,
+              height: 80,
+              decoration: BoxDecoration(
+                color: Colors.grey[200],
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Icon(
+                Icons.image,
+                size: 40,
+                color: Colors.grey[400],
+              ),
+            ),
+            const SizedBox(width: 16.0),
 
