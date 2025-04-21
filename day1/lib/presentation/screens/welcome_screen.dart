b8e79a312085697869b001a04f39d34bbe0bc761
uchini2003 <uchinimarasinghe561@gmail.com>
2025-04-21 15:44:13 +0530

    defines app to user and button navigation

---
+++ day1/lib/presentation/screens/welcome_screen.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import '../widgets/app_button.dart';
+import 'login_screen.dart';
+
+//defines app to user and button to navigate.
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
