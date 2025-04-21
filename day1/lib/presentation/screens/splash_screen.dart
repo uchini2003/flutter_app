7e8285163854bcf4331eeb08d66ec4b2c1fe8f07
uchini2003 <uchinimarasinghe561@gmail.com>
2025-04-21 15:28:56 +0530

    splash screen navigation for 2 sec

---
+++ day1/lib/presentation/screens/splash_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'welcome_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
