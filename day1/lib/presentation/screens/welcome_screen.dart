3e841d0bfc3639018d00f69fe2e2cf1095b42774
uchini2003 <uchinimarasinghe561@gmail.com>
2025-04-21 16:10:31 +0530

    used safe area, padding and spacing

--- day1/lib/presentation/screens/welcome_screen.dart
+++ day1/lib/presentation/screens/welcome_screen.dart
@@ -5,3 +5,44 @@ import 'login_screen.dart';
 //defines app to user and button to navigate.
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    throw UnimplementedError();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        // Using SafeArea
+        body: SafeArea(
+        child: Padding(
+        //padding for edges of screen
+        padding: const EdgeInsets.all(20.0),
+    child: Column(
+    //content center vertically
+    mainAxisAlignment: MainAxisAlignment.center,
+    //children horizontally placed
+    crossAxisAlignment: CrossAxisAlignment.stretch,
+    children: [
+    const Icon(
+    Icons.shopping_cart,
+    size: 100,
+    color: Colors.blue,
+    ),
+    // Spacing
+    const SizedBox(height: 40),
+    //welcome
+    const Text(
+    'Welcome to My Shop',
+    style: TextStyle(
+    fontSize: 28,
+    fontWeight: FontWeight.bold,
+    ),
+    textAlign: TextAlign.center,
+    ),
+
+
+
+
