c5f5ac9d9b3718785e9b410991f6bf376e122f81
uchini2003 <uchinimarasinghe561@gmail.com>
2025-04-22 12:35:00 +0530

    defining app route

--- day1/lib/router/app_router.dart
+++ day1/lib/router/app_router.dart
@@ -4,4 +4,24 @@ import 'package:auto_route/auto route.dart';
 @MaterialAutoRouter(
   replaceInRouteName: 'Screen,Route',
   routes: <AutoRoute>[
+    //all app routes defined
+    AutoRoute(
+      path: '/',
+      page: SplashScreen,
+      initial: true,
+    ),
+    AutoRoute(
+      path: '/welcome',
+      page: WelcomeScreen,
+    ),
+    AutoRoute(
+      path: '/login',
+      page: LoginScreen,
+    ),
+    AutoRoute(
+      path: '/home',
+      page: HomeScreen,
+    ),
+  ],
+)
 
