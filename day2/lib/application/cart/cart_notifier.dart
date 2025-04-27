391e5e4afdebbd7fafd6535dd2df894627db6379
uchini2003 <uchinimarasinghe561@gmail.com>
2025-04-28 00:02:59 +0530

    add and remove products

--- day2/lib/application/cart/cart_notifier.dart
+++ day2/lib/application/cart/cart_notifier.dart
@@ -20,3 +20,45 @@ class CartNotifier extends StateNotifier<CartState> {
     _logUtils.log("dispose");
     super.dispose();
   }
+
+    void addItem(Product product) {
+    _logUtils.log("addItem :: product : $product");
+    
+    final items = {...state.items};
+    
+    if (items.containsKey(product.id)) {
+      items[product.id] = CartItem(
+        product: product,
+        quantity: items[product.id]!.quantity + 1,
+      );
+    } else {
+      items[product.id] = CartItem(product: product);
+    }
+    
+    state = state.copyWith(items: items);
+  }
+
+  void removeItem(String productId) {
+    _logUtils.log("removeItem :: productId : $productId");
+    
+    final items = {...state.items};
+    
+    if (items.containsKey(productId)) {
+      if (items[productId]!.quantity > 1) {
+        items[productId] = CartItem(
+          product: items[productId]!.product,
+          quantity: items[productId]!.quantity - 1,
+        );
+      } else {
+        items.remove(productId);
+      }
+    }
+    
+    state = state.copyWith(items: items);
+  }
+
+  void clear() {
+    _logUtils.log("clear");
+    state = CartState.initial();
+  }
+}
