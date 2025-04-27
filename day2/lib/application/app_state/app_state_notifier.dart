69846ef897f3828259020380d8c12b43f3c63629
uchini2003 <uchinimarasinghe561@gmail.com>
2025-04-27 22:06:44 +0530

    control app main state

--- day2/lib/application/app_state/app_state_notifier.dart
+++ day2/lib/application/app_state/app_state_notifier.dart
@@ -1,25 +1,18 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'presentation/core/app.dart';
+import 'package:dartz/dartz.dart'; // For handling optional values (like maybe having a token or not)
+import 'package:hooks_riverpod/hooks_riverpod.dart'; // For state management
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await ScreenUtil.ensureScreenSize();
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-    ),
-  );
+import '../../domain/core/i_local_repository.dart'; // Local storage interface
+import '../../domain/core/values/constants.dart'; // Keys for storage
+import '../../utils/log_utils.dart'; // For printing logs
+import '../../utils/string_extensions.dart'; // Helpful string functions
+import '../../presentation/routes/app_router.gr.dart'; // App navigation routes
+import '../../presentation/core/app.dart'; // App entry point
+import 'app_state.dart'; // AppState class
 
-  await SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-  ]);
-
-  runApp(
-    const ProviderScope(
-      child: App(),
-    ),
-  );
-}
+// AppStateNotifier controls the app's main state
+class AppStateNotifier extends StateNotifier<AppState> {
+  
+  // Constructor: Takes local storage and sets initial state
+  AppStateNotifier(this._localRepository) : super(AppState.initial()) {
+    _logUtils.log("init"); // Print a log when this is created
+  }
--- day2/lib/main.dart
+++ day2/lib/main.dart
@@ -1,34 +1,25 @@
 import 'package:flutter/material.dart';
-import 'router/app_router.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'presentation/core/app.dart';
 
-//Updated main.dart to use auto_route instead of direct navigation
-void main() {
-  runApp(
-    // added ProviderScope for riverpod state management
-    ProviderScope(
-      child: MyApp(),
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await ScreenUtil.ensureScreenSize();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
     ),
   );
-}
 
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]);
 
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
-  
-  // Created app router instance for auto_route
-  final _appRouter = AppRouter();
-
-  @override
-  Widget build(BuildContext context) {
-    //Changed to MaterialApp.router for auto_route
-    return MaterialApp.router(
-      title: 'My Shop',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        useMaterial3: true,
-      ),
-      routerConfig: _appRouter.config(),
-    );
-  }
+  runApp(
+    const ProviderScope(
+      child: App(),
+    ),
+  );
 }
