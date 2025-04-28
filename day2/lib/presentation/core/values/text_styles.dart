1c8da6e306c61ca4224b5575225f6404b5658fd2
uchini2003 <uchinimarasinghe561@gmail.com>
2025-04-28 11:33:09 +0530

    colors and text styles

---
+++ day2/lib/presentation/core/values/text_styles.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+const sanFranciscoLight = TextStyle(
+  fontFamily: 'SanFranciscoProRegular',
+  fontWeight: FontWeight.w300,
+);
+
+const sanFranciscoRegular = TextStyle(
+  fontFamily: 'SanFranciscoProRegular',
+  fontWeight: FontWeight.w400,
+);
+
+const sanFranciscoMedium = TextStyle(
+  fontFamily: 'SanFranciscoProRegular',
+  fontWeight: FontWeight.w500,
+);
+
+const sanFranciscoSemiBold = TextStyle(
+  fontFamily: 'SanFranciscoProRegular',
+  fontWeight: FontWeight.w600,
+);
+
+const sanFranciscoBold = TextStyle(
+  fontFamily: 'SanFranciscoProRegular',
+  fontWeight: FontWeight.w700,
+);
