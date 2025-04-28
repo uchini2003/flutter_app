f98cc3a52b175ea778bf655e6ea2609e2cd4dd02
uchini2003 <uchinimarasinghe561@gmail.com>
2025-04-28 10:43:48 +0530

    error handling

--- day2/lib/application/app_state/app_state_notifier.dart
+++ day2/lib/application/app_state/app_state_notifier.dart
@@ -1,42 +1,37 @@
-import 'package:dartz/dartz.dart'; // For handling optional values (like maybe having a token or not)
-import 'package:hooks_riverpod/hooks_riverpod.dart'; // For state management
+import 'package:dartz/dartz.dart'; 
+import 'package:hooks_riverpod/hooks_riverpod.dart'; 
 
-import '../../domain/core/i_local_repository.dart'; // Local storage interface
-import '../../domain/core/values/constants.dart'; // Keys for storage
-import '../../utils/log_utils.dart'; // For printing logs
-import '../../utils/string_extensions.dart'; // Helpful string functions
-import '../../presentation/routes/app_router.gr.dart'; // App navigation routes
-import '../../presentation/core/app.dart'; // App entry point
-import 'app_state.dart'; // AppState class
+import '../../domain/core/i_local_repository.dart'; 
+import '../../domain/core/values/constants.dart'; 
+import '../../utils/log_utils.dart'; 
+import '../../utils/string_extensions.dart'; 
+import '../../presentation/routes/app_router.gr.dart'; 
+import '../../presentation/core/app.dart';
+import 'app_state.dart'; 
 
-// AppStateNotifier controls the app's main state
 class AppStateNotifier extends StateNotifier<AppState> {
   
-  // Constructor: Takes local storage and sets initial state
   AppStateNotifier(this._localRepository) : super(AppState.initial()) {
-    _logUtils.log("init"); // Print a log when this is created
+    _logUtils.log("init"); 
   }
 
-  // Set up logging
   static final LogUtils _logUtils = LogUtils(
     featureName: "AppStateNotifier",
     printLog: true,
   );
 
-  // Local storage access
   final ILocalRepository _localRepository;
 
   @override
   void dispose() {
-    _logUtils.log("dispose"); // Print a log when this is destroyed
+    _logUtils.log("dispose"); 
     super.dispose();
   }
 
-    // This function runs when the app starts
+    //runs when the app starts
   Future<void> appStart() async {
     _logUtils.log("appStart :: before state : $state");
 
-    // First, show loading
     state = state.copyWith(
       isLoading: true,
       isAppStarted: none(),
--- day2/lib/router/app_router.gr.dart
+++
@@ -1,9 +0,0 @@
-// dart format width=80
-// GENERATED CODE - DO NOT MODIFY BY HAND
-
-// **************************************************************************
-// AutoRouterGenerator
-// **************************************************************************
-
-// ignore_for_file: type=lint
-// coverage:ignore-file
